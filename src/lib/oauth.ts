cab1b2fde0b3b36c99beb2b7fb481e043b861cc9
ImperialKoi <imperialkoi9@gmail.com>
2025-07-04 12:23:10 -0400

    IDK man

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -3,11 +3,11 @@ import { GitHub, Google } from "arctic";
 export const github = new GitHub(
     process.env.GITHUB_CLIENT_ID ?? "",
     process.env.GITHUB_CLIENT_SECRET ?? "",
-    process.env.DEV === "true" ? "http://localhost:3000/login/github/callback" : "https://www.recesshacks.org/login/github/callback"
+    process.env.DEV === "true" ? "http://localhost:3000/login/github/callback" : "https://recesshacks.org/login/github/callback"
 );
 
 export const google = new Google(
     process.env.GOOGLE_CLIENT_ID ?? "",
     process.env.GOOGLE_CLIENT_SECRET ?? "",
-    process.env.DEV === "true" ? "http://localhost:3000/login/google/callback" : "https://www.recesshacks.org/login/google/callback"
+    process.env.DEV === "true" ? "http://localhost:3000/login/google/callback" : "https://recesshacks.org/login/google/callback"
 );
